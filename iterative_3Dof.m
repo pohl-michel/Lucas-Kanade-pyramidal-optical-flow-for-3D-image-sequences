88659777d4a25dd5a2458c2b54ddc5f14af8eac0
Pohl Michel <64617547+pohl-michel@users.noreply.github.com>
2022-09-18 17:36:28 +0900

    Improved documentation

--- iterative_3Dof.m
+++ iterative_3Dof.m
@@ -1,6 +1,6 @@
 function [ u_temp ] = iterative_3Dof( I, J, u_temp, OF_par )
-% Given the two images I and J and an initial guess u_temp,
-% this function refines the DVF u_temp the deformation vector field between I and J iteratively using the Lucas Kanade method.
+% Given two images I and J and an initial guess of the deformation vector field (DVF) u_temp between I and J,
+% this function refines u_temp iteratively using the Lucas Kanade method.
 %
 % Author : Pohl Michel
 % Date : July 16th, 2020
@@ -9,7 +9,7 @@ function [ u_temp ] = iterative_3Dof( I, J, u_temp, OF_par )
 
     grad_I = spatial_grad_3D(I, OF_par);
     for k = 1:OF_par.nb_iter
-        fprintf('\t\t refinement iteration n°%d \n', k); 
+        fprintf('\t\t refinement iteration nÂ°%d \n', k); 
         u_temp = u_temp + OF_LK_3D( grad_I, translate3DIm(J, u_temp) - I, OF_par);
     end
 
